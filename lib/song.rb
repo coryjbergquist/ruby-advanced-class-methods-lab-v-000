cdec1e13b315c7a1d90783642f02aebea64ad5dc
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-24 22:08:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,11 +50,9 @@ class Song
   def self.new_from_filename(filename)
   a =  filename.split("-").map do |x| x.chomp(".mp3")
   end
-  a
+  
   self.create_by_name(a[1])
     @artist_name = a[0]
     @artist_name
-  binding.pry
-  end
 
 end
