91b16af4ca60ae4e66abfc73ef04d2f3e2256b7d
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-24 22:13:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,9 @@ class Song
   end
 
   def self.create
-  song = self.new
-  @@all << song
-
-      song
+    song = self.new
+      @@all << song
+        song
   end
 
   def self.new_by_name(name)
