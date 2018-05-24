8d12e1991913a66c5c0775b875c96ccd38e4d78b
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-24 21:03:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,8 +39,8 @@ class Song
 end
 
 def self.find_or_create_by_name(name)
-  self.find_by_name(name) if self.name == name
-    self.create_by_name(name) if self.name != name
+  self.find_by_name(name)
+    self.create_by_name(name)
 end
 
 
